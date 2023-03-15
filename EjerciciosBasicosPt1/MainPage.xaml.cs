8c23b5e9c70d7fed5f7ebd5477e2e2ccb9fb76cb
owenramire <owenomar29@gmail.com>
2023-03-14 20:20:55 -0600

    Initial commit

---
+++ EjerciciosBasicosPt1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace EjerciciosBasicosPt1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try {
+			float firstValue = float.Parse(primerValor.Text);
+			float secondValue = float.Parse(segundoValor.Text);
+			float result = firstValue + secondValue;
+            resultado.Text = firstValue + " + " + secondValue +  " = " + result;
+
+        } catch {
+			resultado.Text = "Error al obtener los datos! :c";
+		}
+    }
+}
+
